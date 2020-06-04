4abfb0ea1dd5b79646325e17ff8f134810452014
Yang-yibu <tiandaochouqinyn@163.com>
2020-06-04 19:20:17 +0800

    添加 Table 行内编辑的示例

---
+++ src/views/table/inline-edit-table/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-container">
+    <el-card>
+      <div slot="header">Element-UI 可编辑表格实例</div>
+      <el-edit-table />
+    </el-card>
+
+    <el-card style="margin-top: 20px">
+      <div slot="header">i-view 可编辑表格示例</div>
+      <i-edit-table />
+    </el-card>
+
+    <el-card style="margin-top: 20px">
+      <div slot="header">i-view 可编辑表格示例</div>
+      <i-edit-table1 />
+    </el-card>
+
+  </div>
+</template>
+
+<script>
+import IEditTable from './components/i-edit-table'
+import IEditTable1 from './components/i-edit-table1'
+import ElEditTable from './components/el-edit-table'
+
+export default {
+  name: 'InlineEditTable',
+  components: { IEditTable, ElEditTable, IEditTable1 }
+}
+</script>
