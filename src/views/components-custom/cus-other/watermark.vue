d76af2368fc787ea23624b13d8a7f7d962e724d9
YiBu-M <tiandaochouqinyn163.com>
2020-12-08 10:41:15 +0800

    feat(watermark): 使用水印界面支持取消水印

--- src/views/components-custom/cus-other/watermark.vue
+++ src/views/components-custom/cus-other/watermark.vue
@@ -1,23 +1,45 @@
 <template>
   <div style="width: 400px;">
-    <el-button type="primary" style="margin-top: 10px" size="mini" @click="submit">添加水印</el-button>
+    <el-button type="primary" style="margin-top: 10px" size="mini" @click="submit">{{ hasCusWater? '取消水印': '添加水印' }}</el-button>
   </div>
 </template>
 
 <script>
 import { watermark as cusWaterMatrk } from '@/components/Watermark'
 
+/**
+ * 自动以水印
+ *
+ * 1. 实现可以取消水印方法
+ */
 export default {
   name: 'PageCusWatermark',
   data() {
-    return {}
+    return {
+      hasCusWater: false
+    }
+  },
+  mounted() {
+    if (window.cusWaterMatrk) {
+      this.hasCusWater = true
+    } else {
+      this.hasCusWater = false
+    }
   },
   methods: {
     cusWaterMatrk,
     submit() {
-      this.$message.success('添加水印成功')
-      var a = this.cusWaterMatrk({ text: '' })
-      console.log(a)
+      if (!window.cusWaterMatrk) {
+        this.$message.success('添加水印成功')
+        window.cusWaterMatrk = this.cusWaterMatrk({ text: '' })
+        this.hasCusWater = true
+      } else {
+        window.cusWaterMatrk.isObserver = true
+        const selfNode = document.getElementById(window.cusWaterMatrk.CONTAINERID)
+        selfNode.parentElement.removeChild(selfNode)
+        window.cusWaterMatrk = null
+        this.hasCusWater = false
+      }
     }
   }
 }
